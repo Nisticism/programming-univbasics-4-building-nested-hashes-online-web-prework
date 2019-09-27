50eea3bd250adf3b3ea007aa32ffc086c3864597
Hans Foster <fosterhans@gmail.com>
2019-09-27 03:59:44 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,7 +7,7 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-  {:railroads => {rent_in_dollars: {:key_one => 1, :key_two => 2, :key_three => 3, :key_four => 4}}}
+  {:railroads => {rent_in_dollars: {:names => 1, :key_two => 2, :key_three => 3, :key_four => 4}}}
 end
 
 def monopoly_with_fourth_tier
